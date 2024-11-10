85e3a3a8f3e56b87bc98200667d9fd651e76425b
muhammadrizo <mt936425155@gmail.com>
2024-11-11 00:04:37 +0500

    final js

---
+++ main.js
@@ -0,0 +1,42 @@
+// 1-chi topshiriq
+// let i=0;
+// while (i<=50) {
+//     console.log(i);
+//     i++;
+// }
+// 2-chi topshiriq
+// let i=0;
+// let result=0;
+// while (i<=50) {
+//     result += i;
+//     i++;
+// }
+// console.log(result);
+// 3-chi topshiriq
+// let i = 1;
+// let result = 1;
+// while (i<=50) {
+//     result *= i;
+//     i++;
+// }
+// console.log(result);
+// 4-chi topshiriq
+// let users = ["Tohir", "Sobir", "Jalil", "Bakir"]
+// let i=0;
+// while (i < users.length) {
+//     console.log('salom ${users[i]}')
+//     i++;
+// }
+// 5-chi topshiriq
+// let names = ["Tohir", "Sobir", "Jalil", "Bakir", "Safobek", "Xushnora", "Diyor"];
+// let shortNames = [];
+// let i = 0;
+
+// while (i < names.length) {
+//     if (names[i].length <= 5) {
+//         shortNames.push(names[i]);
+//     }
+//     i++;
+// }
+
+// console.log(shortNames);
